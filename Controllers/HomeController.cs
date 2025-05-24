bed9edc4eaab8d3ffe1ce0a2b8e96df2cf7e66e0
Sebas_u28 <sebastianangulo2003@gmail.com>
2025-05-23 21:14:07 -0500

    se agrego los posts

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,27 +1,29 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using practica.Integration.Exchange;
 using practica.Models;
 
 namespace practica.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
+private readonly ILogger<HomeController> _logger;
+private readonly PostIntegration _postIntegration;
 
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
+public HomeController(ILogger<HomeController> logger, PostIntegration postIntegration)
+{
+    _logger = logger;
+    _postIntegration = postIntegration;
+}
 
-    public IActionResult Index()
-    {
-        return View();
-    }
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+public async Task<IActionResult> Index()
+{
+    var posts = await _postIntegration.GetAllPostsAsync();
+    return View(posts);
+}
+
+
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using practica.Data;
+using practica.Integration.Exchange;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +14,9 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
+
+
+builder.Services.AddScoped<PostIntegration>();
 
 var app = builder.Build();
 
